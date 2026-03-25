eea3f7efb9a5ef361e878bcb0b1365c9d4a61857
LcNguyn72 <nguyenloc080704@gmail.com>
2026-03-25 11:27:42 +0700

    Test

--- Project/FormMain/Program.cs
+++ Project/FormMain/Program.cs
@@ -16,7 +16,7 @@ namespace FormMain
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormMain());
         }
     }
 }
