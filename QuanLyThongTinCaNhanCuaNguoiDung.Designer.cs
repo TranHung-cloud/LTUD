1783a0909c97cd386766689db5050601c067aa3f
nhib2306634-cell <nhib2306634@student.ctu.edu.vn>
2026-04-02 20:56:16 +0700

    Thêm form quản lý thông tin cá nhân người dùng

---
+++ QuanLyThongTinCaNhanCuaNguoiDung.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace LTUD
+{
+    partial class QuanLyThongTinCaNhanCuaNguoiDung
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMaND = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtpNgaySinh = new System.Windows.Forms.DateTimePicker();
+            this.btnLuuThongTin = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtTrangThai = new System.Windows.Forms.TextBox();
+            this.txtVaiTro = new System.Windows.Forms.TextBox();
+            this.txtGiaDinh = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtSoDienThoai = new System.Windows.Forms.TextBox();
+            this.txtXacNhanMK = new System.Windows.Forms.TextBox();
+            this.txtMatKhauMoi = new System.Windows.Forms.TextBox();
+            this.txtMatKhauCu = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnDoiMatKhau = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtMaND
+            // 
+            this.txtMaND.Location = new System.Drawing.Point(852, 114);
+            this.txtMaND.Name = "txtMaND";
+            this.txtMaND.ReadOnly = true;
+            this.txtMaND.Size = new System.Drawing.Size(170, 26);
+            this.txtMaND.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(652, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mã người dùng";
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Location = new System.Drawing.Point(258, 121);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(162, 26);
+            this.txtHoTen.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(91, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tên người dùng";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(91, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ngày Sinh";
+            // 
+            // dtpNgaySinh
+            // 
+            this.dtpNgaySinh.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpNgaySinh.Location = new System.Drawing.Point(258, 180);
+            this.dtpNgaySinh.Name = "dtpNgaySinh";
+            this.dtpNgaySinh.Size = new System.Drawing.Size(200, 26);
+            this.dtpNgaySinh.TabIndex = 5;
+            // 
+            // btnLuuThongTin
+            // 
+            this.btnLuuThongTin.Location = new System.Drawing.Point(744, 419);
+            this.btnLuuThongTin.Name = "btnLuuThongTin";
+            this.btnLuuThongTin.Size = new System.Drawing.Size(86, 36);
+            this.btnLuuThongTin.TabIndex = 6;
+            this.btnLuuThongTin.Text = "Lưu";
+            this.btnLuuThongTin.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(652, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Vai trò";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(652, 170);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Gia đình";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(652, 282);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(156, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Trạng thái hoạt động";
+            // 
+            // txtTrangThai
+            // 
+            this.txtTrangThai.Location = new System.Drawing.Point(852, 278);
+            this.txtTrangThai.Name = "txtTrangThai";
+            this.txtTrangThai.ReadOnly = true;
+            this.txtTrangThai.Size = new System.Drawing.Size(170, 26);
+            this.txtTrangThai.TabIndex = 10;
+            // 
+            // txtVaiTro
+            // 
+            this.txtVaiTro.Location = new System.Drawing.Point(852, 217);
+            this.txtVaiTro.Name = "txtVaiTro";
+            this.txtVaiTro.ReadOnly = true;
+            this.txtVaiTro.Size = new System.Drawing.Size(170, 26);
+            this.txtVaiTro.TabIndex = 11;
+            // 
+            // txtGiaDinh
+            // 
+            this.txtGiaDinh.Location = new System.Drawing.Point(852, 164);
+            this.txtGiaDinh.Name = "txtGiaDinh";
+            this.txtGiaDinh.ReadOnly = true;
+            this.txtGiaDinh.Size = new System.Drawing.Size(170, 26);
+            this.txtGiaDinh.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(91, 239);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(102, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Số điện thoại";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(91, 284);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 20);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Email";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(91, 389);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(104, 20);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Mật khẩu mới";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(91, 337);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(75, 20);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "Mật khẩu";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(91, 435);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(147, 20);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "Xác nhận mật khẩu";
+            // 
+            // txtSoDienThoai
+            // 
+            this.txtSoDienThoai.Location = new System.Drawing.Point(258, 233);
+            this.txtSoDienThoai.Name = "txtSoDienThoai";
+            this.txtSoDienThoai.Size = new System.Drawing.Size(162, 26);
+            this.txtSoDienThoai.TabIndex = 18;
+            // 
+            // txtXacNhanMK
+            // 
+            this.txtXacNhanMK.Location = new System.Drawing.Point(258, 429);
+            this.txtXacNhanMK.Name = "txtXacNhanMK";
+            this.txtXacNhanMK.PasswordChar = '*';
+            this.txtXacNhanMK.Size = new System.Drawing.Size(162, 26);
+            this.txtXacNhanMK.TabIndex = 19;
+            // 
+            // txtMatKhauMoi
+            // 
+            this.txtMatKhauMoi.Location = new System.Drawing.Point(258, 386);
+            this.txtMatKhauMoi.Name = "txtMatKhauMoi";
+            this.txtMatKhauMoi.PasswordChar = '*';
+            this.txtMatKhauMoi.Size = new System.Drawing.Size(162, 26);
+            this.txtMatKhauMoi.TabIndex = 20;
+            // 
+            // txtMatKhauCu
+            // 
+            this.txtMatKhauCu.Location = new System.Drawing.Point(258, 331);
+            this.txtMatKhauCu.Name = "txtMatKhauCu";
+            this.txtMatKhauCu.PasswordChar = '*';
+            this.txtMatKhauCu.Size = new System.Drawing.Size(162, 26);
+            this.txtMatKhauCu.TabIndex = 21;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(258, 281);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(162, 26);
+            this.txtEmail.TabIndex = 22;
+            // 
+            // btnDoiMatKhau
+            // 
+            this.btnDoiMatKhau.Location = new System.Drawing.Point(447, 422);
+            this.btnDoiMatKhau.Name = "btnDoiMatKhau";
+            this.btnDoiMatKhau.Size = new System.Drawing.Size(183, 33);
+            this.btnDoiMatKhau.TabIndex = 23;
+            this.btnDoiMatKhau.Text = "Đổi mật khẩu";
+            this.btnDoiMatKhau.UseVisualStyleBackColor = true;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(233, 28);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(710, 37);
+            this.label12.TabIndex = 24;
+            this.label12.Text = "QUẢN LÝ THÔNG TIN CÁ NHÂN NGƯỜI DÙNG";
+            // 
+            // QuanLyThongTinCaNhanCuaNguoiDung
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1139, 588);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.btnDoiMatKhau);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtMatKhauCu);
+            this.Controls.Add(this.txtMatKhauMoi);
+            this.Controls.Add(this.txtXacNhanMK);
+            this.Controls.Add(this.txtSoDienThoai);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtGiaDinh);
+            this.Controls.Add(this.txtVaiTro);
+            this.Controls.Add(this.txtTrangThai);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnLuuThongTin);
+            this.Controls.Add(this.dtpNgaySinh);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtHoTen);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMaND);
+            this.Name = "QuanLyThongTinCaNhanCuaNguoiDung";
+            this.Text = "QuanLyThongTinCaNhanCuaNguoiDung";
+            this.Load += new System.EventHandler(this.QuanLyThongTinCaNhanCuaNguoiDung_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMaND;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dtpNgaySinh;
+        private System.Windows.Forms.Button btnLuuThongTin;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtTrangThai;
+        private System.Windows.Forms.TextBox txtVaiTro;
+        private System.Windows.Forms.TextBox txtGiaDinh;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtSoDienThoai;
+        private System.Windows.Forms.TextBox txtXacNhanMK;
+        private System.Windows.Forms.TextBox txtMatKhauMoi;
+        private System.Windows.Forms.TextBox txtMatKhauCu;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnDoiMatKhau;
+        private System.Windows.Forms.Label label12;
+    }
+}
---
+++ QuanLyThongTinCaNhanCuaNguoiDung.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient; // Thêm thư viện này để kết nối SQL Server
+
+namespace LTUD
+{
+    public partial class QuanLyThongTinCaNhanCuaNguoiDung : Form
+    {
+        // 1. CHUỖI KẾT NỐI (Bắt buộc: Hãy sửa lại tên Server cho khớp với máy bạn đang mượn)
+        // Thay đổi Database từ QuanLyTaiSan thành QLTaiSanCaNhan
+        string connectionString = @"Server=(localdb)\MSSQLLocalDB; Database=QLTaiSanCaNhan; Integrated Security=True;"; SqlConnection conn;
+
+        // 2. GIẢ LẬP NGƯỜI DÙNG ĐĂNG NHẬP 
+        // (Tạm thời gán bằng ND01, sau này làm chức năng Đăng nhập thì thay bằng biến động)
+        string loggedInUserId = "ND01";
+
+        public QuanLyThongTinCaNhanCuaNguoiDung()
+        {
+            InitializeComponent();
+        }
+
+        // 3. HÀM ĐỔI MÀU GIAO DIỆN (Sử dụng đúng bảng màu nhóm bạn yêu cầu)
+        private void ApplyButtonColors(Button btn)
+        {
+            btn.BackColor = ColorTranslator.FromHtml("#F2EFE7");
+            btn.ForeColor = ColorTranslator.FromHtml("#006A71");
+            btn.FlatStyle = FlatStyle.Flat;
+            btn.FlatAppearance.BorderSize = 0;
+            btn.Font = new Font(btn.Font, FontStyle.Bold);
+        }
+
+        // 4. SỰ KIỆN KHI MỞ FORM LÊN (Chạy đầu tiên)
+        private void QuanLyThongTinCaNhanCuaNguoiDung_Load(object sender, EventArgs e)
+        {
+            this.BackColor = ColorTranslator.FromHtml("#9ACBD0"); // Tô màu nền Form
+
+            // Đổi màu 2 nút bấm (Lưu ý: Tên nút phải khớp với thuộc tính Name bên Designer)
+            ApplyButtonColors(btnLuuThongTin);
+            ApplyButtonColors(btnDoiMatKhau);
+
+            // Gọi hàm lấy dữ liệu đổ lên màn hình
+            LoadThongTinCaNhan();
+        }
+
+        // 5. HÀM TẢI THÔNG TIN CÁ NHÂN LÊN GIAO DIỆN
+        private void LoadThongTinCaNhan()
+        {
+            try
+            {
+                conn = new SqlConnection(connectionString);
+                conn.Open();
+
+                // Lấy thông tin cá nhân và JOIN với bảng Phụ để lấy Tên Vai trò, Tên Gia đình
+                string query = @"
+                    SELECT N.MANGUOIDUNG, N.HOTEN, N.NGAYSINH, N.SODIENTHOAI, N.EMAIL, N.TRANGTHAI,
+                           V.TENVAITRO, G.TENGIADINH
+                    FROM NGUOIDUNG N
+                    LEFT JOIN VAITRO V ON N.MAVAITRO = V.MAVAITRO
+                    LEFT JOIN GIADINH G ON N.MAGIADINH = G.MAGIADINH
+                    WHERE N.MANGUOIDUNG = @MaND";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@MaND", loggedInUserId);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    // Đổ dữ liệu vào các TextBox / DateTimePicker
+                    txtMaND.Text = reader["MANGUOIDUNG"].ToString();
+                    txtHoTen.Text = reader["HOTEN"].ToString();
+                    dtpNgaySinh.Value = Convert.ToDateTime(reader["NGAYSINH"]);
+
+                    // Dùng kiểm tra DBNull.Value để tránh lỗi khi người dùng chưa nhập SĐT hoặc Email
+                    txtSoDienThoai.Text = reader["SODIENTHOAI"] != DBNull.Value ? reader["SODIENTHOAI"].ToString() : "";
+                    txtEmail.Text = reader["EMAIL"] != DBNull.Value ? reader["EMAIL"].ToString() : "";
+
+                    // Đổ dữ liệu vào các ô CHỈ XEM (Vai trò, Gia đình, Trạng thái)
+                    txtVaiTro.Text = reader["TENVAITRO"] != DBNull.Value ? reader["TENVAITRO"].ToString() : "Chưa cấp quyền";
+                    txtGiaDinh.Text = reader["TENGIADINH"] != DBNull.Value ? reader["TENGIADINH"].ToString() : "Cá nhân độc lập";
+                    txtTrangThai.Text = reader["TRANGTHAI"] != DBNull.Value ? reader["TRANGTHAI"].ToString() : "";
+                }
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi tải thông tin: " + ex.Message);
+            }
+        }
+
+        // 6. XỬ LÝ SỰ KIỆN KHI BẤM NÚT "LƯU THÔNG TIN"
+        private void btnLuuThongTin_Click(object sender, EventArgs e)
+        {
+            // Bắt lỗi không cho phép để trống Họ Tên
+            if (string.IsNullOrWhiteSpace(txtHoTen.Text))
+            {
+                MessageBox.Show("Họ tên không được để trống!");
+                return;
+            }
+
+            try
+            {
+                conn.Open();
+                // Chỉ cho phép update 4 trường: Họ Tên, Ngày Sinh, SĐT, Email
+                string query = "UPDATE NGUOIDUNG SET HOTEN = @HoTen, NGAYSINH = @NgaySinh, SODIENTHOAI = @SoDienThoai, EMAIL = @Email WHERE MANGUOIDUNG = @MaND";
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                // Lấy dữ liệu từ giao diện truyền vào câu SQL
+                cmd.Parameters.AddWithValue("@HoTen", txtHoTen.Text);
+                cmd.Parameters.AddWithValue("@NgaySinh", dtpNgaySinh.Value.Date);
+                cmd.Parameters.AddWithValue("@SoDienThoai", txtSoDienThoai.Text);
+                cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+                cmd.Parameters.AddWithValue("@MaND", loggedInUserId);
+
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Cập nhật thông tin cá nhân thành công!");
+                conn.Close();
+
+                // Load lại để hiển thị dữ liệu mới nhất
+
+                LoadThongTinCaNhan();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi cập nhật thông tin: " + ex.Message);
+                conn.Close();
+            }
+        }
+
+        // 7. XỬ LÝ SỰ KIỆN KHI BẤM NÚT "ĐỔI MẬT KHẨU"
+        private void btnDoiMatKhau_Click(object sender, EventArgs e)
+        {
+            // Bước 1: Kiểm tra xem có ô mật khẩu nào bị bỏ trống không
+            if (string.IsNullOrWhiteSpace(txtMatKhauCu.Text) ||
+                string.IsNullOrWhiteSpace(txtMatKhauMoi.Text) ||
+                string.IsNullOrWhiteSpace(txtXacNhanMK.Text))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin mật khẩu!");
+                return;
+            }
+
+            // Bước 2: Kiểm tra mật khẩu mới và xác nhận có khớp nhau không
+            if (txtMatKhauMoi.Text != txtXacNhanMK.Text)
+            {
+                MessageBox.Show("Mật khẩu mới và mật khẩu xác nhận không trùng khớp!");
+                return;
+            }
+
+            try
+            {
+                conn.Open();
+
+                // Bước 3: Lấy mật khẩu cũ từ Database lên để so sánh
+                string checkQuery = "SELECT MATKHAU FROM NGUOIDUNG WHERE MANGUOIDUNG = @MaND";
+                SqlCommand checkCmd = new SqlCommand(checkQuery, conn);
+                checkCmd.Parameters.AddWithValue("@MaND", loggedInUserId);
+                string currentPasswordInDb = checkCmd.ExecuteScalar()?.ToString();
+
+                // Nếu người dùng nhập sai mật khẩu cũ thì chặn lại
+                if (txtMatKhauCu.Text != currentPasswordInDb)
+                {
+                    MessageBox.Show("Mật khẩu hiện tại không chính xác!");
+                    conn.Close();
+                    return;
+                }
+
+                // Bước 4: Nếu mọi thứ đúng hết thì tiến hành update mật khẩu mới
+                string updateQuery = "UPDATE NGUOIDUNG SET MATKHAU = @MatKhauMoi WHERE MANGUOIDUNG = @MaND";
+                SqlCommand updateCmd = new SqlCommand(updateQuery, conn);
+                updateCmd.Parameters.AddWithValue("@MatKhauMoi", txtMatKhauMoi.Text);
+                updateCmd.Parameters.AddWithValue("@MaND", loggedInUserId);
+
+                updateCmd.ExecuteNonQuery();
+                MessageBox.Show("Đổi mật khẩu thành công!");
+
+                // Thành công thì xóa trắng các ô nhập mật khẩu cho an toàn
+                txtMatKhauCu.Clear();
+                txtMatKhauMoi.Clear();
+                txtXacNhanMK.Clear();
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi đổi mật khẩu: " + ex.Message);
+                if (conn.State == ConnectionState.Open) conn.Close();
+
+            }
+        }
+
+        
+    }
+}
